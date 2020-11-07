2d4cce4453cae0363798cbe760980b2a0f3e54b6
ZylleDK <Zylle87@gmail.com>
2020-11-07 11:30:52 +0100

    Initial commit
    Changes to be committed:
    	new file:   Uno/DS1804/.gitignore
    	new file:   Uno/DS1804/.vscode/extensions.json
    	new file:   Uno/DS1804/include/DS1804.h
    	new file:   Uno/DS1804/include/README
    	new file:   Uno/DS1804/lib/README
    	new file:   Uno/DS1804/platformio.ini
    	new file:   Uno/DS1804/src/DS1804.cpp
    	new file:   Uno/DS1804/src/main.cpp
    	new file:   Uno/DS1804/test/README

---
+++ Uno/DS1804/include/DS1804.h
@@ -0,0 +1,50 @@
+/*
+    Copyright 2020 Stefan Nielsen (ZylleDK)
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted 
+    provided that the following conditions are met:
+
+1.  Redistributions of source code must retain the above copyright notice, this list of 
+    conditions and the following disclaimer.
+
+2.  Redistributions in binary form must reproduce the above copyright notice, this list of 
+    conditions and the following disclaimer in the documentation and/or other materials provided 
+    with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+    IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+    OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+    DS1804 is a 100-tap digital potentiometer.
+    This is a driver to easy setup and use this potentiometer together with Arduino.
+
+    This driver is focused at the Arduino Uno (ATMEGA328P)
+    Other Arduinos use the same atmega chip, and the driver can therefor easily be used for these devices 
+    as well as the Arduino Uno. However, I do not provide any guarantee that this driver will work for 
+    your setup, you must ensure that your setup can be used with this driver! 
+*/
+
+#ifndef DS1804_H_
+#define DS1804_H_
+
+#include <Arduino.h>
+#include <stdint.h>
+
+class DS1804
+{
+private:
+    uint8_t _Position = 0;
+    
+
+public:
+    DS1804(/* args */);
+    ~DS1804();
+};
+
+#endif
