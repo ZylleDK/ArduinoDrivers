c898f9fb9e541650b5e3affb9563a7a9bf89b9f5
ZylleDK <Zylle87@gmail.com>
2020-11-07 13:28:38 +0100

    added test program into main.cpp

--- Uno/DS1804/src/main.cpp
+++ Uno/DS1804/src/main.cpp
@@ -1,9 +1,44 @@
 #include <Arduino.h>
+#include "DS1804.h"
 
-void setup() {
-  // put your setup code here, to run once:
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
+DS1804 pot(7, 6, 5);
+  
+uint8_t _pos = 0;
+uint8_t _dir = 1;
+
+int main() 
+{
+
+  while (1)
+  {
+    if (_dir == 1)
+    {
+      if (_pos < 100)
+      {
+        pot.stepUp();
+        _pos++;
+      }
+      else
+      {
+        _dir = 0;
+        pot.stepDown();
+        _pos--;
+      }     
+    }
+    else if (_dir == 0)
+    {
+      if (_pos > 0)
+      {
+        pot.stepDown();
+        _pos--;
+      }
+      else
+      {
+        _dir = 1;
+        pot.stepUp();
+        _pos++;
+      }
+    }
+  }
 }
